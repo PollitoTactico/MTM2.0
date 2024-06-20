e83f52677a36843f97209a761264c7f5fa8ebafe
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-06-19 22:10:01 -0500

    Agregar archivos de proyecto.

---
+++ MTM2.0/Models/BankManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTM2._0.Models
+{
+    internal class BankManager
+    {
+        private static readonly string nombreArchivo = "cuentas_bancarias.json";
+
+        private static List<CuentaBanco> _cuentas;
+        public static List<CuentaBanco> Cuentas
+        {
+            get
+            {
+                if (_cuentas == null)
+                {
+                    CargarCuentasAsync().Wait();
+                }
+                return _cuentas;
+            }
+        }
+
+        public static async Task GuardarCuentasAsync()
+        {
+            await FileHelper.GuardarDatosAsync(nombreArchivo, _cuentas);
+        }
+
+        public static async Task<List<CuentaBanco>> CargarCuentasAsync()
+        {
+            _cuentas = await FileHelper.CargarDatosAsync<CuentaBanco>(nombreArchivo);
+            return _cuentas;
+        }
+
+        public static void BorrarArchivoCuentas()
+        {
+            FileHelper.BorrarArchivo(nombreArchivo);
+            _cuentas = new List<CuentaBanco>();
+        }
+
+        public static bool ExisteNumeroCuenta(string numeroCuenta)
+        {
+            return Cuentas.Any(c => c.NumeroCuenta == numeroCuenta);
+        }
+        public static void ActualizarCuenta(CuentaBanco cuentaActualizada)
+        {
+            var cuentaExistente = Cuentas.FirstOrDefault(c => c.Id == cuentaActualizada.Id);
+            if (cuentaExistente != null)
+            {
+                cuentaExistente.NumeroCuenta = cuentaActualizada.NumeroCuenta;
+                cuentaExistente.NombreTitular = cuentaActualizada.NombreTitular;
+                cuentaExistente.Saldo = cuentaActualizada.Saldo;
+                cuentaExistente.TipoCuenta = cuentaActualizada.TipoCuenta;
+            }
+        }
+    }
+}
---
+++ MTM2.0/Models/CuentaBanco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTM2._0.Models
+{
+    public class CuentaBanco
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("numCuenta")]
+        public string NumeroCuenta { get; set; }
+        [Column("nombreTitular")]
+        public string NombreTitular { get; set; }
+        [Column("saldo")]
+        public decimal Saldo { get; set; }
+        [Column("tipoCuenta")]
+        public string TipoCuenta { get; set; }
+
+
+    }
+}
